47d61c57c9b0dba9562e0dd9e625e4e901412967
Benjamin Heflin and Charles Ewel <benjamin@>
2016-05-31 15:34:55 -0700

    add functional javascript to make flashcards

---
+++ js/scripts.js
@@ -0,0 +1,260 @@
+$(document).ready(function() {
+  $("#javascript").click(function() {
+    $("#a1").toggle();
+    $("#javascript").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a1").click(function() {
+    $("#a1").toggle();
+    $("#javascript").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#operators").click(function() {
+    $("#a2").toggle();
+    $("#operators").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a2").click(function() {
+    $("#a2").toggle();
+    $("#operators").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#variables").click(function() {
+    $("#a3").toggle();
+    $("#variables").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a3").click(function() {
+    $("#a3").toggle();
+    $("#variables").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#naming-conventions").click(function() {
+    $("#a4").toggle();
+    $("#naming-conventions").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a4").click(function() {
+    $("#a4").toggle();
+    $("#naming-conventions").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#functions").click(function() {
+    $("#a5").toggle();
+    $("#functions").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a5").click(function() {
+    $("#a5").toggle();
+    $("#functions").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#methods").click(function() {
+    $("#a6").toggle();
+    $("#methods").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a6").click(function() {
+    $("#a6").toggle();
+    $("#methods").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#arguments").click(function() {
+    $("#a7").toggle();
+    $("#arguments").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a7").click(function() {
+    $("#a7").toggle();
+    $("#arguments").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#parameters").click(function() {
+    $("#a8").toggle();
+    $("#parameters").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a8").click(function() {
+    $("#a8").toggle();
+    $("#parameters").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#return").click(function() {
+    $("#a9").toggle();
+    $("#return").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a9").click(function() {
+    $("#a9").toggle();
+    $("#return").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#chaining-methods").click(function() {
+    $("#a10").toggle();
+    $("#chaining-methods").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a10").click(function() {
+    $("#a10").toggle();
+    $("#chaining-methods").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#strings").click(function() {
+    $("#a11").toggle();
+    $("#strings").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a11").click(function() {
+    $("#a11").toggle();
+    $("#strings").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#booleans").click(function() {
+    $("#a12").toggle();
+    $("#booleans").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a12").click(function() {
+    $("#a12").toggle();
+    $("#booleans").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#undefined").click(function() {
+    $("#a13").toggle();
+    $("#undefined").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a13").click(function() {
+    $("#a13").toggle();
+    $("#undefined").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#NaN").click(function() {
+    $("#a14").toggle();
+    $("#NaN").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a14").click(function() {
+    $("#a14").toggle();
+    $("#NaN").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#Escape").click(function() {
+    $("#a15").toggle();
+    $("#Escape").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a15").click(function() {
+    $("#a15").toggle();
+    $("#Escape").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#Alert").click(function() {
+    $("#a16").toggle();
+    $("#Alert").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a16").click(function() {
+    $("#a16").toggle();
+    $("#Alert").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#Comments").click(function() {
+    $("#a17").toggle();
+    $("#Comments").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a17").click(function() {
+    $("#a17").toggle();
+    $("#Comments").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#Jquery").click(function() {
+    $("#a18").toggle();
+    $("#Jquery").toggle()
+  });
+});
+
+$(document).ready(function() {
+  $("#a18").click(function() {
+    $("#a18").toggle();
+    $("#Jquery").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#Attributes").click(function() {
+    $("#a19").toggle();
+    $("#Attributes").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#a19").click(function() {
+    $("#a19").toggle();
+    $("#Attributes").toggle();
+  });
+});
